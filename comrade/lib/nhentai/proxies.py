60d39c94a5f8eefdf1c7d349cafba8c939417507
Mingde Yin <mingdeyin@gmail.com>
2023-06-03 00:31:11 -0400

    stub implementations

---
+++ comrade/lib/nhentai/proxies.py
@@ -0,0 +1,5 @@
+ORDERED_PROXIES = {
+    "nhentai.net": "https://nhentai.net",
+    "nhentai.to": "https://nhentai.to",
+    "Google Translate Proxy": "http://translate.google.com/translate?sl=ja&tl=en&u=https://nhentai.net",
+}
