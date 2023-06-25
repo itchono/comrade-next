1253e485fa74e7eaee378d88babfc026e056844d
Mingde Yin <mingdeyin@gmail.com>
2023-06-24 23:17:29 -0400

    add relay guild ID to CI

--- comrade/core/configuration.py
+++ comrade/core/configuration.py
@@ -3,9 +3,9 @@ from decouple import config
 # Production configuration
 BOT_TOKEN: str = config("COMRADE_BOT_TOKEN")
 MONGODB_URI: str = config("COMRADE_MONGODB_URI")
+RELAY_GUILD_ID: int = config("COMRADE_RELAY_GUILD_ID", cast=int)
 TIMEZONE: str = config("COMRADE_TIMEZONE", default="UTC")
 ACCENT_COLOUR: int = config("COMRADE_ACCENT_COLOUR", cast=int, default=0xD7342A)
-RELAY_GUILD_ID: int = config("COMRADE_RELAY_GUILD_ID", cast=int)
 
 # Testing-only
 DEV_MODE: bool = config("COMRADE_DEV_MODE", cast=bool, default=False)
